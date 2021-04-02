6c9d8c65cc9cb5eff506f4484c094822387138e8
powerage96 <powerage96@hotmail.com>
2021-04-01 21:15:35 -0300

    ver1

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,23 @@
+import  Expense
+import collections
+import matplotlib.pyplot as plt
+
+
+expenses= Expense.Expenses()
+expenses.read_expenses('C:\\Users\\power\\Documents\\git\\python-collections-budget\\data\\spending_data.csv')
+spending_categories=[]
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+
+spending_counter=collections.Counter(spending_categories)
+
+top5=spending_counter.most_common(5)
+categories, count=zip(*top5)
+
+print(spending_counter)
+print(categories)
+
+fix, ax =plt.subplots()
+ax.bar(categories,count)
+ax.set_title('#')
+plt.show()
